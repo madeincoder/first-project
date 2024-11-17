c528f5765b59501e81e4b4e2cf34bb7a9b12d875
madeincoder <ctgseafood@gmail.com>
2024-11-17 20:53:27 +0600

    quickpack setup

---
+++ src/app/config/index.ts
@@ -0,0 +1,9 @@
+import donenv from 'dotenv';
+import path from 'path';
+
+donenv.config({ path: path.join((process.cwd(), '.env')) });
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
